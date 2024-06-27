f23bbb0277a7f292d4826153c8330e3a628d10fc
suresh k <115919438+Binary-Shade@users.noreply.github.com>
2024-06-27 17:42:30 +0530

    init commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const EventEmitter = require('node:events');
+const logEvents = require('./logEvent');
+
+class MyEmitter extends EventEmitter {}
+
+const myEmitter = new MyEmitter();
+myEmitter.on('logs', (msg) => {
+  logEvents(msg)
+});
+myEmitter.emit('logs', 'log emitted !');
